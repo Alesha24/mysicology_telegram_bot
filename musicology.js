c908d63432407e2ddd76d5f861f5a6728b78660b
Alesha24 <31233485+Alesha24@users.noreply.github.com>
2018-05-13 20:42:30 +0200

    Update musicology.js

--- musicology.js
+++ musicology.js
@@ -1,31 +1,17 @@
 var TelegramBot = require('node-telegram-bot-api');
-var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.send('it is running\n');
+}).listen(process.env.PORT || 5000);
 
-// Устанавливаем токен, который выдавал нам бот.
 var token = '585154658:AAEuYv4NXwZ1YS_M3Gf-bj-BoT6BolT5VPw';
-// Включить опрос сервера
 var bot = new TelegramBot(token, {
     polling: true
 });
 
-//var jazz_collection = ['https://soundcloud.com/zer0day'];
-//var house_collection =[];
-//var triphop_collection = [];
-//bot.on('message', (msg) => {
-//
-//    var Jazz = "jazz";
-//    var House = "house";
-//    var trip_hop = "trip-hop"
-//
-//    if (msg.text.toString().toLowerCase().indexOf(Jazz) === 0) {
-//        bot.sendMessage(msg.chat.id, jazz[0]);
-//    } else if (msg.text.toString().toLowerCase().indexOf(House) === 0) {
-//        bot.sendMessage(msg.chat.id, house[0]);
-//    } else if (msg.text.toString().toLowerCase().indexOf(trip_hop) === 0) {
-//        bot.sendMessage(msg.chat.id, trip_hop[0]);
-//    }
-//
-//});
 var jazz_collection = [];
 var house_collection = [];
 var techno_collection = [];
@@ -42,24 +28,6 @@ bot.onText(/\/start/, (msg) => {
 
 var count = 0;
 
-//bot.on('message', (msg) => {
-//    var jazz = "jazz";
-//    if (msg.text.toString().toLowerCase().indexOf(jazz) === 0) {
-//        bot.sendMessage(msg.chat.id, "Here is your jazz song:");
-//    }
-//    var house = "house";
-//    if (msg.text.toString().toLowerCase().includes(bye)) {
-//        bot.sendMessage(msg.chat.id, "Here is your house track:");
-//    }
-//    var techno = "techno";
-//    if (msg.text.indexOf(robot) === 0) {
-//        bot.sendMessage(msg.chat.id, "Here is your techno song:");
-//    }
-//    var electro = "electro";
-//    if (msg.text.indexOf(electro) === 0) {
-//        bot.sendMessage(msg.chat.id, "Here is your electro song:");
-//        bot.sendMessage(msg.chat.id, electro_collection[Math.floor(Math.random() * items.length)]);
-//    }
 var rand = function () {
     return Math.floor(Math.random() * electro_collection.length);
 }
