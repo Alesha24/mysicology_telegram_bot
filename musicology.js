d1928fa2618a2e7df01efb4410107dfada565f97
Alesha24 <31233485+Alesha24@users.noreply.github.com>
2018-05-13 20:31:02 +0200

    Update musicology.js

--- musicology.js
+++ musicology.js
@@ -1,4 +1,5 @@
 var TelegramBot = require('node-telegram-bot-api');
+var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 
 // Устанавливаем токен, который выдавал нам бот.
 var token = '585154658:AAEuYv4NXwZ1YS_M3Gf-bj-BoT6BolT5VPw';
